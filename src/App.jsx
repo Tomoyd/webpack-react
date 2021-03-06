a64ab91acde7b4f811ead91e0a2e420d5ca3f8b6
Tomoyd <ydh9419@163.com>
2021-03-06 17:11:15 +0800

    [eslinty]

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { Suspense } from "react";
 import { BrowserRouter, Switch } from "react-router-dom";
 import Router from "./router";
 import StoreProvider from "./store";
-
+import React from "react";
 const App = () => {
-  const b = 1 + "";
   return (
     <Suspense fallback={null}>
       <BrowserRouter>
