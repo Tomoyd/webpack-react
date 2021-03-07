b493d691ec6477d52aa7399b1475e981a3be70c6
Tomoyd <ydh9419@163.com>
2021-03-07 19:46:27 +0800

    [es]

---
+++ es/Button/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.less";
+
+var Button = function Button() {
+  return /*#__PURE__*/React.createElement("div", null, "button");
+};
+
+export default Button;
---
+++ es/Text/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./index.less";
+
+var Text = function Text() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: styles.text
+  }, "text");
+};
+
+export default Text;
