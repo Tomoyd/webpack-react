a862a934483715542621df223a26188020a35890
Tomoyd <ydh9419@163.com>
2021-03-07 12:06:07 +0800

    reset

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: [
-    // Plugins for PostCSS
-    ["postcss-short", { prefix: "x" }],
-    "postcss-preset-env"
-  ]
-};
--- src/components/Text/index.jsx
+++
@@ -1,6 +0,0 @@
-import styles from "./index.less";
-const Text = () => {
-  return <div className={styles.text}>text123</div>;
-};
-
-export default Text;
